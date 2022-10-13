65bb0f19faf7c906aa0a008d8ae627a114866761
jessica.suarez@col-leoburnett.om-WebMaster <115501549+Jessica0705WebMaster@users.noreply.github.com>
2022-10-13 16:01:32 -0500

    Update header.php

--- src/wp-content/themes/unoraya/header.php
+++ src/wp-content/themes/unoraya/header.php
@@ -16,16 +16,80 @@
     <link rel="stylesheet" href="<?php echo $unorayaCss; ?>" async defer>
     <?php $buildJs = esc_url( get_template_directory_uri() . '/js/build.js' ); ?>
     <script src="<?php echo $buildJs; ?>"></script>
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-K3234SQ');</script>
-    <!-- End Google Tag Manager -->
+	
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-KNBPM36');</script>
+	<!-- End Google Tag Manager -->
+	
+	<!-- Global site tag (gtag.js) - Google Ads: 862755000  Google Ad Grants-->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=AW-862755000"></script>
+	<script>
+  		window.dataLayer = window.dataLayer || [];
+  		function gtag(){dataLayer.push(arguments);}
+  		gtag('js', new Date());
+
+  		gtag('config', 'AW-862755000');
+	</script>
+	
+	<!-- Global site tag (gtag.js) - Google Ads: 624525641 Google ADS-->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=AW-624525641"></script>
+	<script>
+  		window.dataLayer = window.dataLayer || [];
+  		function gtag(){dataLayer.push(arguments);}
+  		gtag('js', new Date());
+
+  		gtag('config', 'AW-624525641');
+	</script>
+	<!-- End Google ADS -->
+	
+	<!-- Google tag (gtag.js)  PIXEL APX GOOGLE ANALYTICS-->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-121077039-7"></script>
+	<script>
+  		window.dataLayer = window.dataLayer || [];
+  		function gtag(){dataLayer.push(arguments);}
+  		gtag('js', new Date());
+ 
+  		gtag('config', 'UA-121077039-7');
+	</script>
+	
+	<!-- Meta Pixel Code  PIXEL APX FACEBOOK ADS-->
+	<script>
+		!function(f,b,e,v,n,t,s)
+		{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+		n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+		if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+		n.queue=[];t=b.createElement(e);t.async=!0;
+		t.src=v;s=b.getElementsByTagName(e)[0];
+		s.parentNode.insertBefore(t,s)}(window, document,'script',
+		'https://connect.facebook.net/en_US/fbevents.js');
+		fbq('init', '1028910734356537');
+		fbq('track', 'PageView');
+	</script>
+	<!-- End Meta Pixel Code -->
+	
 </head>
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
+	
+	<!-- Google Tag Manager (noscript) NOSCRIPT COMPLEMENTO GOOGLE TAG MANAGER-->
+	<noscript>
+		<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KNBPM36"
+		height="0" width="0" style="display:none;visibility:hidden"></iframe>
+	</noscript>
+	<!-- End Google Tag Manager (noscript) -->
+	
+	<!-- Facebook Ads (noscript) NOSCRIPT COMPLEMENTO FACEBOOK ADS APX-->
+	<noscript>
+		<img height="1" width="1" style="display:none"
+		src="https://www.facebook.com/tr?id=1028910734356537&ev=PageView&noscript=1"
+	/></noscript>
+	<!-- End Facebook Ads (noscript) -->
+	
+	
     <header class="header">
       <div id="menu_fixed" class="fixed-this">
          <nav class="navbar navbar-expand-lg"
