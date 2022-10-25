2a0308360fc268ce00bb7709d34045fca695dc31
jessica.suarez@col-leoburnett.om-WebMaster <115501549+Jessica0705WebMaster@users.noreply.github.com>
2022-10-25 11:41:48 -0500

    Update header.php

--- src/wp-content/themes/unoraya/header.php
+++ src/wp-content/themes/unoraya/header.php
@@ -144,7 +144,9 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K3234SQ"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
-<!-- facebook pixel (noscript) -->
+	
+	
+<!-- Facebook Pixel (noscript) -->
 <noscript><img height="1" width="1" style="display:none"
 src="https://www.facebook.com/tr?id=545571769782600&ev=PageView&noscript=1"
 /></noscript>
