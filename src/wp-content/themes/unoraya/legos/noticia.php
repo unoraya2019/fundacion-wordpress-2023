82c19f684aab34baa15f01a5cb473c04f2649c3e
carlos@unoraya.com - Líder de Proyectos <46873964+unoraya2019@users.noreply.github.com>
2023-05-30 11:22:12 -0500

    Update noticia.php

--- src/wp-content/themes/unoraya/legos/noticia.php
+++ src/wp-content/themes/unoraya/legos/noticia.php
@@ -1,7 +1,5 @@
 <?php
     $imageNt = get_sub_field('banner');
-    $urlNt = $imageNt['url'];
-    $altNt = $imageNt['alt'];
 ?>
 <?php $buildNoticiasCss = esc_url( get_template_directory_uri() . '/css/legos/noticias.css' ); ?>
 <link rel="stylesheet" href="<?php echo $buildNoticiasCss; ?>" async defer>
@@ -10,8 +8,8 @@
     <div class="row">
       <div class="col-md-8 offset-md-2">
         <h1><?php echo get_sub_field('titulo'); ?></h1>
-        <img src="<?php echo esc_url($urlNt); ?>"
-             alt="<?php echo esc_attr($altNt); ?>"
+        <img src="<?php echo $imageNt; ?>"
+             alt="<?php echo get_sub_field('titulo'); ?>"
              class="img-fluid">
         <div class="description">
           <?php echo get_sub_field('contenido'); ?>
