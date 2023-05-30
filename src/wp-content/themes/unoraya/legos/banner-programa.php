1893ead7953f0afc7320038e4164b32357055764
carlos@unoraya.com - Líder de Proyectos <46873964+unoraya2019@users.noreply.github.com>
2023-05-30 11:15:52 -0500

    Update banner-programa.php

--- src/wp-content/themes/unoraya/legos/banner-programa.php
+++ src/wp-content/themes/unoraya/legos/banner-programa.php
@@ -1,34 +1,26 @@
 <?php
     $imageBPr = get_sub_field('banner');
-    $urlBPr = $imageBPr['url'];
-    $altBPr = $imageBPr['alt'];
-    
-    $imageBPrRes = get_sub_field('banner_responsive');
-    $urlBPrRes = $imageBPrRes['url'];
-    $altBPrRes = $imageBPrRes['alt'];
-    
+    $imageBPrRes = get_sub_field('banner_responsive');    
     $imageBPrLogo = get_sub_field('logo');
-    $urlBPrLogo = $imageBPrLogo['url'];
-    $altBPrLogo = $imageBPrLogo['alt'];
 ?>
 <?php $buildBannerProgramaCss = esc_url( get_template_directory_uri() . '/css/legos/banner-programa.css' ); ?>
 <link rel="stylesheet" href="<?php echo $buildBannerProgramaCss; ?>" async defer>
 <section class="banner-programa aflora-bnr-se position-relative after-po">
     <div class="img-prt position-absolute">
-        <img src="<?php echo esc_url($urlBPr); ?>"
-             alt="<?php echo esc_attr($altBPr); ?>"
+        <img src="<?php echo esc_url($imageBPr); ?>"
+             alt="banner programa"
              class="object-fit-cover">
     </div>
     <div class="img-prt d-none-n d-block-767 position-absolute">
-      <img src="<?php echo esc_url($urlBPrRes); ?>"
-           alt="<?php echo esc_attr($altBPrRes); ?>"
+      <img src="<?php echo esc_url($imageBPrRes); ?>"
+           alt="banner programa versión celular"
            class="object-fit-cover">
     </div>
     <div class="d-flex align-items-center some-txt-logo position-relative">
     <div>
         <div data-wow-duration="2s" class="log-prt wow bounceInDown">
-            <img src="<?php echo esc_url($urlBPrLogo); ?>"
-               alt="<?php echo esc_attr($altBPrLogo); ?>"
+            <img src="<?php echo esc_url($imageBPrLogo); ?>"
+               alt="logo de programa"
                class="img-fluid">
         </div>
       <div data-wow-duration="2s" class="wow bounceInUp">
